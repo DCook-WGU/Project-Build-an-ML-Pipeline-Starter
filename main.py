6f043a07c3948e37ffb16dc8cf1fa600b675d545
Caprikey <dcoo230@wgu.edu>
2025-10-25 04:17:17 +0000

    run failed. Error in data_check

--- main.py
+++ main.py
@@ -76,7 +76,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+ 
+             _ = mlflow.run(
+                os.path.join(root_path, "src/data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"], 
+                    "min_price": float(config["etl"]["min_price"]), 
+                    "max_price": float(config["etl"]["max_price"])
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -84,6 +84,39 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert np.isfinite(kl_div) and kl_div < kl_threshold
 
 
+
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+
+def test_row_count(data: pd.DataFrame):
+
+    """
+    Test if dataset is over an appropriate size, not too small and not too large. 
+    
+    Args:
+        data: Current dataset to test
+        
+    Raises:
+        AssertionError: If row count is less than 15,000 and greater than 1,000,000
+    """
+
+    assert 15000 < data.shape[0] < 1000000
+
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test if price is within the defined range.
+    
+    Args:
+        data: Current dataset to test
+        min_price: Minimum allowed price
+        max_price: Maximum allowed price
+        
+    Raises:
+        AssertionError: If an item's price range is outside of the defined range. 
+    """
+
+    assert data['price'].between(min_price, max_price, inclusive=True).all()
