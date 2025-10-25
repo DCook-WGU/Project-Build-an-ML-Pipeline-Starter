33d245d7949813f3e3f9e9de53f756e04dc17d5e
Caprikey <dcoo230@wgu.edu>
2025-10-25 04:44:17 +0000

    error with main.py, copy and paste error with mlflow source

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def go(config: DictConfig):
             ##################
 
             _ = mlflow.run(
-                os.path.join(root_path, "src/data_check"),
+                os.path.join(root_path, "src/train_random_forest"),
                 "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
