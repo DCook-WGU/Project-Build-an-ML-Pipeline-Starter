e9d3ac262c2c76d7cc501e6556e6abc220c0b810
Caprikey <dcoo230@wgu.edu>
2025-10-26 20:48:51 +0000

    update to sitecustomize.py for warning suppression, parent process still has the config depreciated warning.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,4 +1,24 @@
 import warnings
+
+# Kill MLflow gateway deprecation early, no Pydantic import needed
+warnings.filterwarnings(
+    "ignore",
+    message=r"Support for class-based `config` is deprecated.*"
+)
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    module=r"mlflow(\.|$)"
+)
+
+# Optional Warning, enabling for now
+# Quiet any FutureWarnings inside mlflow
+warnings.filterwarnings(
+    "ignore",
+    category=FutureWarning,
+    module=r"mlflow(\.|$)"
+)
+
 try:
     from pydantic.warnings import (
         UnsupportedFieldAttributeWarning,
@@ -8,3 +28,4 @@ try:
     warnings.filterwarnings("ignore", category=PydanticDeprecatedSince20)
 except Exception:
     pass
+
